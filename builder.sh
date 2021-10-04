3eba037bf79786a007cff1984924b910e2b8d856
kx1t <ramonkolb@gmail.com>
2021-10-04 13:50:55 -0400

    updates

--- builder.sh
+++ builder.sh
@@ -18,13 +18,13 @@ git pull
 # make the build certs root_certs folder:
 # Note that this is normally done as part of the github actions - we don't have those here, so we need to do it ourselves before building:
 #ls -la /etc/ssl/certs/
-mkdir -p ./root_certs/etc/ssl/certs
-mkdir -p ./root_certs/usr/share/ca-certificates/mozilla
-mkdir -p ./rootfs/root
+#mkdir -p ./root_certs/etc/ssl/certs
+#mkdir -p ./root_certs/usr/share/ca-certificates/mozilla
+#mkdir -p ./rootfs/root
 
-cp -P /etc/ssl/certs/*.crt ./root_certs/etc/ssl/certs
-cp -P /etc/ssl/certs/*.pem ./root_certs/etc/ssl/certs
-cp -P /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
+#cp -P /etc/ssl/certs/*.crt ./root_certs/etc/ssl/certs
+#cp -P /etc/ssl/certs/*.pem ./root_certs/etc/ssl/certs
+#cp -P /usr/share/ca-certificates/mozilla/*.crt ./root_certs/usr/share/ca-certificates/mozilla
 
 #echo "$(git branch --show-current)_($(git rev-parse --short HEAD))_$(date +%y-%m-%d-%T%Z)" > rootfs/root/branch
 
